0ad7dbad7974cea184e2d02ce4765963af0f8c27
Pieter Janssen <4687577+pajter@users.noreply.github.com>
2024-12-05 00:12:02 +0100

    add day 3.b

--- src/3/b.ts
+++ src/3/b.ts
@@ -2,7 +2,12 @@ const input = Deno.readTextFileSync(`${import.meta.dirname}/input.txt`)
   .trim()
   .replaceAll(/\s*/gmi, "");
 
-const muls = input.matchAll(/mul\(([\d]{1,3}),\s*([\d]{1,3})\)/gmi);
+const inputStripped = input.replaceAll(
+  /don't\(\)(.*?(?=do\(\)))do\(\)|don't\(\).*$/gmi,
+  "",
+);
+
+const muls = inputStripped.matchAll(/mul\(([\d]{1,3}),\s*([\d]{1,3})\)/gmi);
 
 let sum = 0;
 for (const mul of muls) {
