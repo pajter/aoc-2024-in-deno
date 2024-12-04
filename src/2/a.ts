758e232b769ee6fb9fdb9d40d1737c0b031b5dfb
Pieter Janssen <4687577+pajter@users.noreply.github.com>
2024-12-04 21:44:50 +0100

    add day 2.a

--- src/2/a.ts
+++ src/2/a.ts
@@ -1,5 +1,31 @@
 import { readInput } from "../util.ts";
 
-const input = readInput<number[][]>(`${import.meta.dirname}/input.test.txt`);
+const input = readInput<number[][]>(`${import.meta.dirname}/input.txt`);
 
-console.log(input);
+const map = new Map<number[], {
+  distances: number[];
+  safe: boolean;
+}>();
+
+for (const item of input) {
+  const distances = item.map((num, k) => {
+    const next = item[k + 1];
+
+    if (typeof next === "undefined") {
+      return;
+    }
+
+    return num - next;
+  }).filter((v) => typeof v === "number");
+
+  const safe = distances.some((v) => v === 0 || Math.abs(v) > 3) === false &&
+    (distances.every((v) => v > 0) || distances.every((v) => v < 0));
+
+  map.set(item, { distances, safe });
+}
+
+const sum = map.values().reduce((acc, { safe }) => {
+  return acc + (safe ? 1 : 0);
+}, 0);
+
+console.log(sum);
